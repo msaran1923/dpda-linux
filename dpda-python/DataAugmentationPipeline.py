2e4cb95e226f1f3614fdbf9014d031d6256c01fc
Efe Çiftci <efeciftci@gmail.com>
2023-12-13 20:20:17 +0300

    improved pipelining mechanism

--- dpda-python/DataAugmentationPipeline.py
+++ dpda-python/DataAugmentationPipeline.py
@@ -1,9 +1,21 @@
+from DataAugmenterDistributionPreserving import DataAugmenterDistributionPreserving
+from ImageLoaderOpenCV import ImageLoaderOpenCV
+from ImageSaverOpenCV import ImageSaverOpenCV
+from NoiseGeneratorPerlin import NoiseGeneratorPerlin
+
 class DataAugmentationPipeline:
     def __init__(self):
-        self.dataAugmenters = []
+        imageLoader = ImageLoaderOpenCV()
+        imageSaver = ImageSaverOpenCV()
+        noiseGenerator = NoiseGeneratorPerlin()
+        DPDA_Power = 1.0
+        dataAugmenterDistributionPreserving = DataAugmenterDistributionPreserving(imageLoader, imageSaver, noiseGenerator, DPDA_Power)
+        self.dataAugmenters = [dataAugmenterDistributionPreserving]
 
-    def execute(self, input_directory, output_directory, image_path, augmentation_count, scale_factor, percentage):
-        augmentationPipeline = DataAugmentationPipeline()
-        # augmentationPipeline.add(DataAugmentationDistributionPreserving(), 100)
-        # augmentationPipeline.add(DataAugmenterSmoothRandomErase(), 50)
-        # augmentationPipeline.add(DataAugmenterRandomErase(), 100)
+    def appendToPipeline(self, nextAugmenter):
+        self.dataAugmenters.append(nextAugmenter)
+        if len(self.dataAugmenters) > 1:
+            self.dataAugmenters[-2].pipelineDataAugmenter = self.dataAugmenters[-1]
+
+    def execute(self, inputDirectory, outputDirectory, imagePath, augmentationCount, scaleFactor, percentage):
+        self.dataAugmenters[0].execute(inputDirectory, outputDirectory, imagePath, augmentationCount, scaleFactor, percentage)
